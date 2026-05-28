712cb010fe8956b277c35cf407f066ba1c7b1fe2
rifalsumarsono <rifalsumarsono99@gmail.com>
2026-05-29 02:20:46 +0700

    menambahkan cara dan fitur

---
+++ exceptions/custom_exceptions.py
@@ -0,0 +1,11 @@
+class KursusPenuhError(Exception):
+    pass
+
+class PesertaSudahTerdaftarError(Exception):
+    pass
+
+class KursusTidakDitemukanError(Exception):
+    pass
+
+class PesertaTidakDitemukanError(Exception):
+    pass
